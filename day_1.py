aeb54c80fd93b8722467810102d6856404890667
DaviVilarinho <davi.r.vilarinho@gmail.com>
2023-12-04 21:07:20 -0300

    day 1 pt 2

--- day_1.py
+++ day_1.py
@@ -14,13 +14,75 @@ def get_calibration_value(input_line: str):
             break
         except ValueError:
             pass
-        
+
     return only_numbers
+
+
+STR_TO_NUM = {
+    "zero": 0,
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9
+}
+
+
+def is_str_number(inp: str):
+    return STR_TO_NUM.get(inp.lower(), -1)
+
+
+def get_first_number(inp: str):
+    for i, ch in enumerate(inp):
+        for j in range(i):
+            number_represented = is_str_number(inp[j:i])
+            if number_represented >= 0:
+                return number_represented
+        try:
+            return int(ch)
+        except ValueError:
+            pass
+
+    return -1
+
+
+def get_last_number(inp: str):
+    leninp = len(inp)
+    i = leninp - 1
+    while i >= 0:
+        for j in range(leninp - i):
+            number_represented = is_str_number(inp[i:(leninp-j)])
+            if number_represented >= 0:
+                return number_represented
+        try:
+            return int(inp[i])
+        except ValueError:
+            pass
+        i -= 1
+    return -1
+
+
+def get_calibration_value_2(input_line: str):
+    fst = get_first_number(input_line)
+    fst = fst if fst >= 0 else 0
+    snd = get_last_number(input_line)
+    snd = snd if snd >= 0 else 0
+    return fst * 10 + snd
+
 
 def sum_calibration_values(input_text: str):
     return sum(list(map(get_calibration_value, input_text.split('\n'))))
 
-def test():
+
+def sum_calibration_values_2(input_text: str):
+    return sum(list(map(get_calibration_value_2, input_text.split('\n'))))
+
+
+def test_pt1():
     input_text = """1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
@@ -28,7 +90,22 @@ treb7uchet"""
     assert sum_calibration_values(input_text) == 142
     print("passou")
 
+
+def test_pt2():
+    input_text = """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"""
+    assert sum_calibration_values_2(input_text) == 281
+    print("passou")
+
+
 if __name__ == '__main__':
-    #test()
+    #    test_pt2()
     with open('day_1_input.txt', 'r') as input_file:
-        print(sum_calibration_values(input_file.read()))
+        inputstr = input_file.read()
+        print(sum_calibration_values(inputstr))
+        print(sum_calibration_values_2(inputstr))
