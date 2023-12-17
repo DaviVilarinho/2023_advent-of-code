ba08fea82fd435443a659bcc196f9b4bf0e74265
DaviVilarinho <davi.r.vilarinho@gmail.com>
2023-12-17 14:20:30 -0300

    day 9 p2 done

--- day_9.js
+++ day_9.js
@@ -32,13 +32,39 @@ const sol1 = (input) => {
     return extrapolateSum;
 }
 
+const sol2 = (input) => {
+    const lines = input.split('\n').map(line => line.split(' ').map(x => parseInt(x)));
+    let extrapolateAtBegin = 0;
+    for (let oasisReport of lines) {
+        let deltas = [oasisReport].concat(recursivelyConstructDeltas(oasisReport));
+        deltas[deltas.length - 1] = [0, ...deltas[deltas.length - 1]];
+        for (let i = deltas.length - 2; i >= 0; i--) {
+            deltas[i] = [deltas[i][0] - deltas[i + 1][0], ...deltas[i]];
+        }
+        extrapolateAtBegin += deltas[0][0];
+    }
+    return extrapolateAtBegin;
+}
+
+
+
 const test_1 = () => {
     const input = `0 3 6 9 12 15
 1 3 6 10 15 21
 10 13 16 21 30 45
-`
-    console.log(sol1(input), 114)
+`;
+    console.log(sol1(input), 114);
+}
+
+const test_2 = () => {
+    const input = `0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+`;
+    console.log(sol2(input), 2);
 }
 
 test_1();
 console.log(`Part 1: ${sol1(fs.readFileSync('day_9_input.txt', 'utf8'))}`);
+test_2();
+console.log(`Part 2: ${sol2(fs.readFileSync('day_9_input.txt', 'utf8'))}`);
